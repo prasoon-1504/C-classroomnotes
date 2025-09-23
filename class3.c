c8d4147b7f30eefa10105db1412678fede785d20
Prasoon Kumar <prasoon_baller@Prasoons-MacBook-Air.local>
2025-09-23 21:06:55 +0530

    class 3 first commit

---
+++ class3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+  //?overall idea of data structures
+  //!Abstract data types
+
+  //? List:
+  //* can insert data, modify and search it is available in python
+  //* for example insert(A), remove(B), search(B), etc...
+
+  //? Stack:
+  //* ek k upar ek data  to store karne ko stack bola jata hai// it is a data structure that stores data in LIFO format
+  //* the last element added is the fist one to be removed or accessed also know as LIFO(last in first one)
+  //*operations: Push: to add elements, POP: to remove from last element, PEEK: to view an element, isempty: to check if a stack is empty
+
+  //? Queue:
+  //*it is a horizontal data structure where the data is stored in a horizontal manner left side is called front and right side is called rear
+  //*operations: 
+  //*Enqueue: used to insert element *the element always enters from the rear
+  //* Dequeue: used to remove element *the element is always remove from the front
+  //*isempty: used to check if the queue is empty or not
+  //*size: used to check the size of the data structure
+  return 0;
+}
